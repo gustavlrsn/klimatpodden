48a7caa0d1ae70f16df168280bdc920adca6c5aa
Gustav Larsson <gustav.larsson@gmail.com>
2019-10-16 20:36:38 +0200

    Add exportPathMap to generate static pages for posts and pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
+const fs = require("fs");
+
 module.exports = {
+  exportPathMap: async () => {
+    const pageSlugs = fs
+      .readdirSync("./content/pages/")
+      .map(fileName => fileName.substring(0, fileName.length - 3));
+
+    const postSlugs = fs
+      .readdirSync("./content/posts/")
+      .map(fileName => fileName.substring(0, fileName.length - 3));
+
+    const pages = [...pageSlugs, ...postSlugs].reduce(
+      (pages, { slug }) =>
+        Object.assign({}, pages, {
+          [`/${slug}`]: {
+            page: "/[slug]",
+            query: { slug }
+          }
+        }),
+      {}
+    );
+
+    return Object.assign({}, pages, {
+      "/": { page: "/" }
+    });
+  },
   webpack: cfg => {
     cfg.module.rules.push({
       test: /\.md$/,
