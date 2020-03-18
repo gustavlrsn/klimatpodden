c3a7beb181d301489aed6cbba3f337e413029b5b
Gustav Larsson <gustav.larsson@gmail.com>
2020-03-18 21:52:16 +0100

    Fix links to static resources

--- components/head.js
+++ components/head.js
@@ -10,37 +10,34 @@ export default ({
     <link
       rel="apple-touch-icon"
       sizes="180x180"
-      href="/static/favicons/apple-touch-icon.png"
+      href="/favicons/apple-touch-icon.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="32x32"
-      href="/static/favicons/favicon-32x32.png"
+      href="/favicons/favicon-32x32.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="16x16"
-      href="/static/favicons/favicon-16x16.png"
+      href="/favicons/favicon-16x16.png"
     />
-    <link rel="manifest" href="/static/favicons/site.webmanifest" />
+    <link rel="manifest" href="/favicons/site.webmanifest" />
     <link
       rel="mask-icon"
-      href="/static/favicons/safari-pinned-tab.svg"
+      href="/favicons/safari-pinned-tab.svg"
       color="#5bbad5"
     />
-    <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+    <link rel="shortcut icon" href="/favicons/favicon.ico" />
     <meta name="msapplication-TileColor" content="#da532c" />
-    <meta
-      name="msapplication-config"
-      content="/static/favicons/browserconfig.xml"
-    />
+    <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
     <meta name="description" content={description} />
     <meta name="theme-color" content="#ffffff" />
     <meta
       property="og:image"
-      content="https://klimatpodden.se/static/klimatpodden-og.png"
+      content="https://klimatpodden.se/klimatpodden-og.png"
     />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="1200" />
@@ -51,7 +48,7 @@ export default ({
     <meta name="twitter:description" content={description} />
     <meta
       name="twitter:image"
-      content="https://klimatpodden.se/static/klimatpodden-og.png"
+      content="https://klimatpodden.se/klimatpodden-og.png"
     />
     <meta name="twitter:card" content="summary_large_image" />
   </Head>
