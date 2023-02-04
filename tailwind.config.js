f94fae91b4f250951aab35709fc3739a4d87fb96
Gustav Larsson <gustav.larsson@gmail.com>
2023-02-04 13:29:55 +0000

    Update tailwind, add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./content/**/*.md",
+  ],
   theme: {
     extend: {
       gridTemplateColumns: {
